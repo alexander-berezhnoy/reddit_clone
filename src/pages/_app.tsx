949399e2e08bf84bc08bbc58f3ed9b9462d89a08
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2023-01-26 02:37:17 +0200

    Add navbar and logos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
@@ -5,10 +6,14 @@ import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
 
+import Layout from "@/src/components/Layout";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
